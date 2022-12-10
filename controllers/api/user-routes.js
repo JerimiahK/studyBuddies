6639d1d58ceec2dc72d12a9b016d3a84ed3a4319
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-12-10 10:40:01 -0700

    added user-route file, and seeds for it to test

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// Create New User
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      user_name: req.body.user_name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.post for Login
+
+// router.post for Logout
