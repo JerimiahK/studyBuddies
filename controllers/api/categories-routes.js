daeab6fc12d9c19477e90311e905f03456f75c3e
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-12-11 20:34:57 -0700

    added the get all categories route

--- controllers/api/categories-routes.js
+++ controllers/api/categories-routes.js
@@ -1,11 +1,23 @@
 // Categories Routes
+const router = require("express").Router();
+const { Category, Subtopic } = require("../../models");
 
 // http:localhost:3001/api/categories
 //GET
+router.get("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Subtopic }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // http:localhost:3001/api/categories/:id
 //POST
-
+router.post("/:id", (req, res) => {});
 // http:localhost:3001/api/categories/
 //PUT
 
