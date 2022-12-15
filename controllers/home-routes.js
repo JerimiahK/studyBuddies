1e63e6609f5e7481a65a0ae9b8239e82e79d437c
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-12-14 19:29:45 -0700

    working on targeting topic cards when clicked

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Topics, Categories } = require("../models");
+const { Topics, Categories, Subtopics } = require("../models");
 
 // http:localhost:3001/
 //GET
@@ -15,9 +15,23 @@ router.get("/", async (req, res) => {
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
+//// http:localhost:3001/subtopics
+router.get("/subtopics/:id", async (req, res) => {
+  try {
+    const subtopicsData = await Subtopics.findByPk(req.params.id, {
+      include: [{ model: Topics }],
+    });
+    const subtopics = subtopicsData.get({ plain: true });
+    res.render("subtopic", {
+      subtopics,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
