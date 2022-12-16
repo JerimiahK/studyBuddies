ad6e4ff24ece8431b598b21f37471c91086060b6
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-12-15 18:51:20 -0700

    took console logs out of login.js so it doesnt console log passwords and emails publicly

--- public/js/login.js
+++ public/js/login.js
@@ -5,8 +5,7 @@ const loginFormHandler = async (event) => {
   const password = $("#password").val();
   //   $.trim(email);
   //   $.trim(password);
-  console.log(email);
-  console.log(password);
+
 
   if (email && password) {
     const response = await fetch("/api/users/login", {
