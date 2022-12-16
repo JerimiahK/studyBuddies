77199697b48af47d8348e74066058add18af47be
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-12-16 09:30:39 -0700

    made userID static in subtopic routes

--- controllers/api/subtopics-routes.js
+++ controllers/api/subtopics-routes.js
@@ -3,11 +3,10 @@ const router = require("express").Router();
 const { Subtopics, Topics, SubtopicStatus, Users } = require("../../models/");
 
 router.get("/", async (req, res) => {
-  // const topicID = req.query.id;
-  // console.log();
-  const topicID = 1;
+  const topicID = req.query.id;
   // const currentUser = req.session.id;
-  const userID = req.session.id;
+  const userID = 1;
+  // console.log(req.session.id);
 
   try {
     const subtopicData = await Subtopics.findAll({
@@ -29,7 +28,6 @@ router.get("/", async (req, res) => {
         },
       ],
     });
-    console.log(subtopicData);
     const subtopics = subtopicData.map((subtopic) => {
       const user = subtopic.users[0].subtopicStatus;
       return {
@@ -43,7 +41,6 @@ router.get("/", async (req, res) => {
         demo_code: subtopic.demo_code,
       };
     });
-    console.log(subtopics);
     res.status(200).render("subtopic", {
       subtopics,
       loggedIn: req.session.loggedIn,
